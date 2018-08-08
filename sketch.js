ea3d971447c1608b7e7d04ce822860cecc558824
Oluchukwu Okafor <ojokafor@wpi.edu>
2018-08-08 15:35:42 -0400

    coords

--- sketch.js
+++ sketch.js
@@ -55,7 +55,7 @@ function setup() {
 		dragging = false;
 		mouseX=event.pageX - canvas.offsetLeft;
 		mouseY=event.pageY - canvas.offsetTop;
-		document.getElementById("gamestate").innerHTML = "E";
+		document.getElementById("gamestate").innerHTML = "X "+mouseX+" Y "+mouseY;
 		if(istouching){
 			board.mousedown([mouseX,mouseY]);
 		}
